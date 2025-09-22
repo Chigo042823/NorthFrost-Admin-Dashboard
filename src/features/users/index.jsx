82cb3046315a74a1424cecee897092f3127b6618
Altheo Pacheco <altheopacheco564@gmail.com>
2025-09-22 16:13:51 +0800

    case sensitivity fix in path

--- src/features/users/index.jsx
+++ src/features/users/index.jsx
@@ -1,5 +1,5 @@
 import { MainContainer } from "@/shared/components/mainContainer"
-import { UsersTable } from "@/features/users/components/usersTable"
+import { UsersTable } from "@/features/users/components/UsersTable"
 
 export const Users = () => {
     return (

5ec893cc9dccb156a4964fdfaa2cdd8fb458ae64
Altheo Pacheco <altheopacheco564@gmail.com>
2025-09-26 00:26:23 +0800

    Logo src path fix

--- src/features/navbar/components/Branding.jsx
+++ src/features/navbar/components/Branding.jsx
@@ -5,7 +5,7 @@ export const Branding = () => {
     return (
         <div className="flex items-center gap-x-2 w-[30vw] h-max-full md:w-full md:mb-2 ml-2 md:ml-0 md:pb-3 md:border-b border-b-stone-400 
             text-3xl font-bold text-indigo-600 text-shadow-2xs">
-            <img src="../public/northfrost-logo.jpg" alt="logo" />
+            <img src="/northfrost-logo.jpg" alt="logo" />
         </div>
     )
 }
